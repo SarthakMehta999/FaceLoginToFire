da9db61eb214c81d815c9213ccc00c084f5f46f4
Deceree Quiamco <decereequiamco@gmail.com>
2018-04-26 10:16:04 +0800

    jump

---
+++ app/src/main/java/fiture/quiamco/com/homefiture/Ectomorph/Day1ChestTriceps/Dips.java
@@ -0,0 +1,15 @@
+package fiture.quiamco.com.homefiture.Ectomorph.Day1ChestTriceps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fiture.quiamco.com.homefiture.R;
+
+public class Dips extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dips);
+    }
+}
