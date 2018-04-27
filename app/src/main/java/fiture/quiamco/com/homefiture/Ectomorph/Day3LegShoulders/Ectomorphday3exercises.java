f8c4705cd0312ad86d2ccb5d87a4d18588016494
ShawnErl <shawnerlsala@bsit.usjr.edu.ph>
2018-04-27 16:40:53 +0800

    Tara oh

---
+++ app/src/main/java/fiture/quiamco/com/homefiture/Ectomorph/Day3LegShoulders/Ectomorphday3exercises.java
@@ -0,0 +1,15 @@
+package fiture.quiamco.com.homefiture.Ectomorph.Day3LegShoulders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fiture.quiamco.com.homefiture.R;
+
+public class Ectomorphday3exercises extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ectomorphday3exercises);
+    }
+}
---
+++ app/src/main/java/fiture/quiamco/com/homefiture/Ectomorph/LayoutEcto/ectomorphday1exercises.java
@@ -0,0 +1,15 @@
+package fiture.quiamco.com.homefiture.Ectomorph.LayoutEcto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fiture.quiamco.com.homefiture.R;
+
+public class ectomorphday1exercises extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ectomorphday1exercises);
+    }
+}
