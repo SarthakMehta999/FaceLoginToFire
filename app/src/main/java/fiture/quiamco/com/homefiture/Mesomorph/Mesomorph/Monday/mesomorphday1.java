63e381da67f1ad3191c8a3fb2d85466c0acd35f0
ShawnErl <shawnerlsala@bsit.usjr.edu.ph>
2018-04-27 17:00:03 +0800

    MamC

---
+++ app/src/main/java/fiture/quiamco/com/homefiture/Mesomorph/Mesomorph/Monday/mesomorphday1.java
@@ -0,0 +1,15 @@
+package fiture.quiamco.com.homefiture.Mesomorph.Mesomorph.Monday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fiture.quiamco.com.homefiture.R;
+
+public class mesomorphday1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mesomorphday1);
+    }
+}
