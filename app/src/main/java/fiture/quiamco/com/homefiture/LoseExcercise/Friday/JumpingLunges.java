1e6ce5dff00cbc76807aacc6d2ae0313f286d8ae
Deceree Quiamco <decereequiamco@gmail.com>
2018-04-30 17:49:43 +0800

    Added Labels of EXERCISES

--- app/src/main/java/fiture/quiamco/com/homefiture/LoseExcercise/Friday/JumpingLunges.java
+++ app/src/main/java/fiture/quiamco/com/homefiture/LoseExcercise/Friday/JumpingLunges.java
@@ -18,5 +18,7 @@ public class JumpingLunges extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jumping_lunges);
+
+
     }
 }
